3f88ad7dd6b505b1238d55f689f6be0b2da03ff8
Vrishank Sharma <sharma08462@gmail.com>
2024-04-15 15:52:30 +0530

    list render

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
+import { useState } from "react";
 
 function App() {
-  
+  const [items, setItems] = useState([
+    { id: crypto.randomUUID(), name: "Item 1" },
+    { id: crypto.randomUUID(), name: "Item 2" },
+  ]);
 
-  return(
+  function addItem() {
+    setItems((currentItems) => {
+      return [{ id: crypto.randomUUID(), name: "New Item" },...currentItems ];
+    });
+  }
+
+  return (
     <>
-    <h1>Rendering List</h1>
+      <h1>Rendering List</h1>
+
+      <div>
+        <button onClick={addItem}>Add Item</button>
+        <pre>{JSON.stringify(items, null, 2)}</pre>
+      </div>
+
+      {/* To convert array to React JSX elements */}
+
+      <pre>
+        {items.map((item) => {
+          return <div key={item.id}>
+            {item.name}
+            <input type="text"/>
+            </div>;
+        })}
+      </pre>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
